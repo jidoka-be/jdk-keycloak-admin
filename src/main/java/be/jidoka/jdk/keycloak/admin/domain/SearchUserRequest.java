2def8630e01b47c8cc01d62cfa79ac401e0caf70
Martijn Haex <martijn.haex@jidoka.be>
2020-07-02 14:14:21 +0200

    REFACTOR: review add javadoc to clarify where the search term will be used for.

--- src/main/java/be/jidoka/jdk/keycloak/admin/domain/SearchUserRequest.java
+++ src/main/java/be/jidoka/jdk/keycloak/admin/domain/SearchUserRequest.java
@@ -6,6 +6,13 @@ import java.util.Optional;
 
 public interface SearchUserRequest {
 
+	/**
+	 * @return Search term that will be used to search (partially) in:
+	 * username,
+	 * firstName,
+	 * lastName,
+	 * or email fields
+	 */
 	String getSearch();
 
 	Optional<String> getClientId();
